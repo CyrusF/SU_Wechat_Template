7d7c12108883e6f81958533150fc0dbd1c2dea32
Cyrus Fang <fcs98@sina.com>
2017-03-04 01:28:07 +0800

    Add files via upload

---
+++ kanban.py
@@ -0,0 +1,64 @@
+import xlrd
+import os
+def dabiaoti(x):
+  print("<div id=\"js_content\" class=\"rich_media_content \"><section data-role=\"outer\" label=\"Powered by 135editor.com\" style=\"font-family: 微软雅黑; font-size: 16px;\"><section class=\"_135editor\" data-tools=\"135编辑器\" data-id=\"87845\" style=\"font-size: medium; border-width: 0px; border-style: none; border-color: initial; box-sizing: border-box;\"><section data-width=\"20em\" style=\"margin: 10px auto; width: 20em; text-align: center;\"><section style=\"padding: 6px; background-color: rgb(172, 29, 16); color: rgb(255, 255, 255);transform: rotate(360deg);-webkit-transform: rotate(360deg);-moz-transform: rotate(360deg);-ms-transform: rotate(360deg);-o-transform: rotate(360deg);\"><section style=\"padding: 10px; border-width: 1px; border-style: dashed; border-color: white; box-sizing: border-box;\"><strong><span class=\"135brush\" data-brushtype=\"text\">",file=f)
+  print(x,file=f)
+  print("</span></strong></section></section><section data-width=\"20em\" class=\"\" style=\"margin-top: -1.8em; background-image: url(\"http://mmbiz.qpic.cn/mmbiz_png/v3uv5bVvkfB6uYP1kSIBL0Zf7AiajzKmS3VPbX4yibLiczaNqVBasQjtxICDkGx3VLChjTichQM1nlFWJc9hA8gEXQ/0?wx_fmt=png\"); background-repeat: no-repeat; background-size: 100%%; width: 20em; height: 3em;\"><br/></section></section></section><p style=\"white-space: normal;\"><br/></p>",file=f)
+def xueyuan(x):
+    print("<section class=\"_135editor\" data-tools=\"135编辑器\" data-id=\"32290\" data-color=\"rgb(172, 29, 16)\" data-custom=\"rgb(128, 177, 53)\" style=\"font-size: medium; border-width: 0px; border-style: none; border-color: initial; box-sizing: border-box;\"><section style=\"margin-top: 5px; margin-bottom: 5px; clear: both; border-width: 0px; border-style: none; border-color: initial; box-sizing: border-box;\"><section style=\"border-top: 2.5px solid rgb(172, 29, 16); border-right-color: rgb(172, 29, 16); border-bottom-color: rgb(172, 29, 16); border-left-color: rgb(172, 29, 16); font-size: 1em; font-weight: inherit; text-decoration: inherit; color: rgb(255, 255, 255); box-sizing: border-box;\"><section style=\"margin-top: 2px; border-width: 0px; border-style: initial; border-color: rgb(172, 29, 16); overflow: hidden; color: inherit; box-sizing: border-box;\"><section style=\"display: inline-block; font-size: 1em; font-weight: inherit; text-align: inherit; text-decoration: inherit; color: inherit; border-color: rgb(172, 29, 16); box-sizing: border-box;\"><section class=\"135brush\" data-bcless=\"darken\" data-brushtype=\"text\" style=\"padding: 5px 10px; display: inline-block; line-height: 1.4em; height: 32px; vertical-align: top; font-weight: bold; float: left; color: inherit; border-color: rgb(79, 13, 7); background: rgb(172, 29, 16); box-sizing: border-box;\"><strong>",file=f)
+    print(x,file=f)
+    print("</strong></section><section style=\"display: inline-block; vertical-align: top; width: 0px; height: 0px; border-top: 32px solid rgb(172, 29, 16); border-right: 32px solid transparent; border-top-right-radius: 4px; border-bottom-left-radius: 2px; color: inherit; box-sizing: border-box;\"></section></section></section></section></section></section>",file=f)
+def bianhao(x):
+    print("<section class=\"_135editor\" data-tools=\"135编辑器\" data-id=\"86463\" data-color=\"rgb(172, 29, 16)\" data-custom=\"rgb(128, 177, 53)\" style=\" font-size: medium; border-width: 0px; border-style: none; border-color: initial;  box-sizing: border-box; \"><section style=\"margin-right: 25px; margin-left: 25px; padding-top: 25px; padding-bottom: 25px; padding-left: 25px; color: inherit; font-size: 16px; border-left: 2px solid rgb(172, 29, 16); box-sizing: border-box;\"><section style=\"margin-left: -43px; padding: 3px; width: 34px; height: 34px; text-align: center; border-radius: 20px; background-color: rgb(254, 254, 254); box-sizing: border-box;\"><section style=\"padding: 2px; width: 28px; height: 28px; border-radius: 20px; border-width: 2px; border-style: solid; border-color: rgb(172, 29, 16); box-sizing: border-box;\"><section style=\"width: 20px; height: 20px; line-height: 1.5em; border-radius: 16px; font-size: 14px; color: rgb(255, 255, 255); background: rgb(172, 29, 16); box-sizing: border-box;\"><span style=\"font-size: 16px;\">",file=f)
+    print(x,file=f)
+    print("</span></section></section></section>",file=f)
+def xiaobiaoti(x,y):
+    print("<section style=\"margin-top: -30px; color: inherit;\"><section style=\"margin-left: -0.6em;\"><p style=\"white-space: normal; font-size: 14px; color: inherit;\"><span style=\"color: #AC1D10; font-size: 16px;\"><strong class=\"135brush\" data-brushtype=\"text\" style=\"color: inherit;\">--",file=f)
+    print(y,x,file=f)
+    print("</strong></span></p></section>",file=f)
+def shijiandidian(x,y):
+    print("<p style=\"white-space: normal; font-size: 14px; color: inherit;\"><br/></p><p style=\"white-space: normal; color: inherit; font-size: 14px;\"><span style=\"font-size: 16px;\"><strong style=\"color: inherit;\">时间：",file=f)
+    print(x,file=f)
+    print("</strong><br/></span></p><p style=\"white-space: normal; color: inherit; line-height: 1.5em; font-size: 14px;\"><span style=\"font-size: 16px;\"><strong>地点：",file=f)
+    print(y,file=f)
+    print("</strong></span></p>",file=f)
+def jieshao(x):
+    print("<p style=\"white-space: normal; line-height: 1.5em; font-size: 14px; color: inherit;\"><br/></p><p style=\"white-space: normal; line-height: 1.5em; font-size: 14px; color: inherit;\"><span style=\"font-size: 16px;\">",file=f)
+    print(x,file=f)
+    print("</span></p><p style=\"white-space: normal; line-height: 1.5em; font-size: 14px; color: inherit;\"><br/></p></section></section></section>",file=f)
+
+os.system("chcp 437")
+print("自动生成的看板不包含图片，")
+print("可复制到为微信公众号后台使用。")
+print("====Coding by Cyrus====")
+inputfilename="input3.xlsx" #输入文件名
+outputfilename="output3.html"  #输出文件名
+
+f=open(outputfilename,"w")
+data=xlrd.open_workbook(inputfilename)
+table=data.sheets()[0]
+title=table.cell(0,1).value
+x=title
+i=1
+
+dabiaoti(x)
+while x!="看板结束":
+    a=table.cell(i,0).value
+    x=table.cell(i,1).value
+    if a=="学院":
+        print("正在处理 %s"%(x))
+        xueyuan(x)
+        j=1
+    else:
+        sj=table.cell(i,2).value
+        dd=table.cell(i,3).value
+        js=table.cell(i,4).value
+        bianhao(j)
+        xiaobiaoti(a,x)
+        shijiandidian(sj,dd)
+        jieshao(js)
+        j+=1
+    i+=1
+
+print("已生成看板 %s"%(title))
+os.system("pause")
